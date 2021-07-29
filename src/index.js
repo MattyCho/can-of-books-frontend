3cec1a9015a81aa4bb2365886e2f3246b6854204
MattyCho <myc.chosen@gmail.com>
2021-07-28 17:54:37 -0700

    added a form and functions to add/delete favorite books

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import {Auth0Provider} from '@auth0/auth0-react';
 
-
-// TODO: wrap everything in Auth0
 ReactDOM.render(
   <Auth0Provider domain="dev-3zy4mv8n.us.auth0.com" clientId="rJEBl55FkKaA1rHQwCBuWTLl2xGekl77" redirectUri="http://localhost:3000/">
     <App />
