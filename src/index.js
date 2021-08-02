41104cd3bed26dde19656af63f4188365a29f73f
MattyCho <myc.chosen@gmail.com>
2021-08-01 18:45:32 -0700

    changed localhost redirect URI

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './components/App';
 import {Auth0Provider} from '@auth0/auth0-react';
 
 ReactDOM.render(
-  <Auth0Provider domain="dev-3zy4mv8n.us.auth0.com" clientId="rJEBl55FkKaA1rHQwCBuWTLl2xGekl77" redirectUri="http://localhost:3000/">
+  <Auth0Provider domain="dev-3zy4mv8n.us.auth0.com" clientId="rJEBl55FkKaA1rHQwCBuWTLl2xGekl77" redirectUri="https://mc-can-of-books-frontend.netlify.app/">
     <App />
   </Auth0Provider>, document.getElementById('root')
   );
